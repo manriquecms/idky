4dade7e07df1aed8684a21e6205103e19860f019
manriquecms <manrique.cms@gmail.com>
2018-07-20 22:35:07 +0200

    Improved tests to refactor some checks and include the free from fail functionality

--- mafia/src/test/java/org/manriquecms/mafia/service/MafiaServiceUT.java
+++ mafia/src/test/java/org/manriquecms/mafia/service/MafiaServiceUT.java
@@ -10,9 +10,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.UUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MafiaServiceUT {
@@ -81,11 +80,11 @@ public class MafiaServiceUT {
     }
 
     @Test
-    public void memberToJailNoSameLevel() {
+    public void memberToJailNoSameLevelAndFreeFromJail() {
         mafiaService.clearFamily();
-        //|   m1   |          |  m1 |
-        //|   m2   |    =>    |  m3 |
-        //|m3 m4 m5|          |m4 m5|
+        //|   m1   |          |  m1 |         |   m1   |
+        //|   m2   |    =>    |  m3 |    =>   |   m2   |
+        //|m3 m4 m5|          |m4 m5|         |m3 m4 m5|
         var m1 = Member.randomMember();
         var m2 = Member.randomMember(m1.getId());
         var m3 = Member.randomMember(m2.getId());
@@ -94,10 +93,29 @@ public class MafiaServiceUT {
         List.of(m1,m2,m3,m4,m5).forEach(member -> mafiaService.addMember(member));
 
         mafiaService.memberToJail(m2.getId());
-        Assert.assertNotEquals("m4's boss has changed",m4.getIdBoss(),m2.getId());
-        Assert.assertEquals("m4 and m5 has the same new boss",m4.getIdBoss(),m5.getIdBoss());
-        var newBoss = mafiaService.getMember(m4.getIdBoss());
 
-        Assert.assertTrue("the new boss was the oldest subordinate",newBoss.getJoinDate().compareTo(mafiaService.getOldestSubordinate(newBoss.getId()).getJoinDate()) <= 0);
+        var seemsOldest = List.of(m3,m4,m5).stream().sorted(Comparator.comparing(Member::getJoinDate)).findFirst().get();
+        Assert.assertTrue("m1 has now 1 subordinate",m1.getSubordinates().size() == 1);
+        Assert.assertTrue("m1 is boss from seemsOldest",checkMemberIsBossFromOther(m1,seemsOldest));
+        Assert.assertTrue("seemsOldest has now 2 subordinates",seemsOldest.getSubordinates().size() == 2);
+        m2.getSubordinates().stream().filter(uuid -> !uuid.equals(seemsOldest.getId())).forEach(id -> {
+            Assert.assertTrue("seemsOldest is boss from "+id,checkMemberIsBossFromOther(seemsOldest,mafiaService.getMember(id)));
+        });
+
+        mafiaService.freeMemberFromJail(m2.getId());
+        Assert.assertTrue("m1 has again 1 subordinate",m1.getSubordinates().size() == 1);
+        Assert.assertTrue("m1 is boss from m2",checkMemberIsBossFromOther(m1,m2));
+        Assert.assertTrue("m2 has again 3 subordinates",m2.getSubordinates().size() == 3);
+        Assert.assertTrue("m2 is boss from m3",checkMemberIsBossFromOther(m2,m3));
+        Assert.assertTrue("m2 is boss from m3",checkMemberIsBossFromOther(m2,m4));
+        Assert.assertTrue("m2 is boss from m3",checkMemberIsBossFromOther(m2,m5));
+
     }
+
+    private boolean checkMemberIsBossFromOther(Member boss, Member subordinate){
+        return boss.getSubordinates().contains(subordinate.getId())
+                && subordinate.getIdBoss().equals(boss.getId());
+    }
+
+
 }
